2af5e5fa07eeef50afac33869ec1fdd1fefb4862
Bogdan Logachev <logachyovbogdan@gmail.com>
2023-08-16 11:36:16 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	let newBrackets = new Map(bracketsConfig);
+	let stack = [];
+
+	str.split("").forEach((elem) => {
+		if (elem == newBrackets.get(stack[stack.length - 1])) {
+			stack.pop();
+		} else {
+			stack.push(elem);
+		}
+	});
+	return stack.length !== 0 ? false : true;
+};
